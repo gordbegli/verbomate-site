f0d8c5f9283e789e8faa8c279e247ca06d156798
gordbegli <ggordbegli@gmail.com>
2024-01-22 14:19:28 -0500

    save

--- app/GitHub.js
+++ app/GitHub.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function GitHub() {
     return (
-        <Link className="text-xs md:text-sm mt-4 md:mt-6 block bg-gray-300 hover:bg-gray-400 text-white font-bold py-1 px-2 rounded max-w-xs md:max-w-none w-full text-center h-min iw" href="https://github.com/gordbegli/verbomate" target="_blank" passHref>
+        <Link className="text-xs md:text-sm mt-4 md:mt-6 block bg-gray-300 hover:bg-gray-400 text-white font-bold py-1 px-2 rounded max-w-xs md:max-w-none w-full text-center h-min iw" href="https://github.com/gordbegli/verbomate__" target="_blank" passHref>
             View the Code on GitHub
         </Link>
     )
